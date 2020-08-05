5ff6cf8c36b080e7978ebf6f9a34678dbba79cc0
Daniel Ilich <ukqfhf@yandex.ru>
2020-08-05 22:39:21 +0300

    Updates

---
+++ precache-manifest.9b4729963a8d9b626b6793b54c84a8a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca9739e3ab6b07187b1cd3ba806a74c",
+    "url": "/TOTSystems-test/index.html"
+  },
+  {
+    "revision": "27019e3542400aaec259",
+    "url": "/TOTSystems-test/static/css/main.79dd0793.chunk.css"
+  },
+  {
+    "revision": "49ced86e046ed0c76416",
+    "url": "/TOTSystems-test/static/js/2.cb9fdee4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TOTSystems-test/static/js/2.cb9fdee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27019e3542400aaec259",
+    "url": "/TOTSystems-test/static/js/main.c5e14620.chunk.js"
+  },
+  {
+    "revision": "5c48cc98378858d62b7c",
+    "url": "/TOTSystems-test/static/js/runtime-main.6c4cf1b0.js"
+  }
+]);
